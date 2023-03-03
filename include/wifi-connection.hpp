f3f523dfdc86b54b9a5b86f00f3f5fb123cb169d
TheRealArthurDent <TheRealArthurDent@users.noreply.github.com>
2023-03-03 20:41:40 +0100

    Added some doxygen docu.

--- include/wifi-connection.hpp
+++ include/wifi-connection.hpp
@@ -6,6 +6,15 @@
 #include <vector>
 #include "wifi-dependent.hpp"
 
+/**
+ * \brief A controller for a WiFi connection.
+ *
+ * A singleton that will take care of your WiFi connection. It will try to
+ * establish a connection, attempt reconnects in case of a connection loss
+ * and inform registered \c WifiDependent s about successful connects as well
+ * as connection losses by calling their \c onWifiConnectionEstablished or
+ * \c onWifiConnectionLost callback respectively.
+ */
 class WifiConnection
 {
   WiFiEventHandler wifiConnectHandler;
@@ -17,6 +26,9 @@ class WifiConnection
 
   // Singleton "constructor"
 public:
+  /**
+   * Get's the \c Singleton instance.
+   */
   static auto getInstance() -> WifiConnection &
   {
     static WifiConnection instance;
@@ -33,13 +45,33 @@ private:
 
   // normal methods
 public:
+  /**
+   * Returns the WiFi connection status.
+   *
+   * \return \c true in case the WiFi connection is established
+   * or \c false otherwise.
+   */
   auto isConnected() -> bool;
-  void init();
+
+  /**
+   * Initializes the controller while registering a collection of
+   * \c WifiDependent s.
+   *
+   * \param dependents A colletion of instances that depend on a WiFi
+   * connection. The dependents will be informed about changes in the
+   * connection's status.
+   */
   void init(std::list<WifiDependent *> dependents);
   // void addDependent
   // void removeDependent
 
 private:
+  /**
+   * An init method, that does not register any dependents.
+   * It's currently private since no add or remove methods for
+   * \c WifiDependents exist. Might become \c public in case that changes.
+   */
+  void init();
   void connect();
   void setConnected(bool connected);
   void notifyDependentsConnected();
