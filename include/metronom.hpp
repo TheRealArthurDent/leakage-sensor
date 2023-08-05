d0fca4574e2c541607546687cea562d04cce081e
TheRealArthurDent <TheRealArthurDent@users.noreply.github.com>
2023-08-06 00:39:54 +0200

    Minor fix to Metronom.

--- include/metronom.hpp
+++ include/metronom.hpp
@@ -5,14 +5,12 @@
  *
  * A helper class that let's you manage (somewhat) constant durations
  * of your loop() function. Define a cycleDuration and call waitForNextCycle()
- * at the end of your
- *
- *
+ * at the end of your loop.
+ * Easy, isn't it? :)
  */
 class Metronom
 {
 public:
-    Metronom() = default;
     /**
      * \brief Constructor.
      *
@@ -24,41 +22,31 @@ public:
      * In case a cycle has been missed - due to stuff happening within the loop
      * taking more time than the defined cycle duration - this bool defines
      * whether the missed cycle should be skipped or not.
-     * In this case \c true will make metronom's
-     * \c waitForNextCycle just wait until the next regular cycle,
-     * thereby skipping at least one cycle, whereas \c false will
-     * make metronom's \c waitForNextCycle return immediately and
-     * thereby trigger the next iteration of the loop in order to make up the
-     * delay.
-     *
+     * In this case \c true will make metronom's \c waitForNextCycle just wait
+     * until the next regular cycle, thereby skipping at least one cycle,
+     * whereas \c false will make metronom's \c waitForNextCycle return
+     * immediately and thereby trigger the next iteration of the loop in order
+     * to make up the delay.
      * \warning Using \c skipMissedCycles = \c false in conjunction with a loop
      * that usually takes more time than \c cycleDuration will lead to a
      * growing delay that can never be made up.
      * \endparblock
      */
     Metronom(int cycleDuration, bool skipMissedCycles = true) : cycleDuration(cycleDuration), skipMissedCycles(skipMissedCycles){};
-    Metronom(Metronom const &);            // Don't implement.
-    Metronom &operator=(Metronom const &); // Don't implement.
 
     /**
      * Blocks until the next cycle starts.
      * Might return immediately if the Metronom has been configured NOT to skip
-     * missed cycles.
-     * \see Metronom(int, bool)
+     * missed cycles ( \c skipMissedCycles = \c false ).
+     * See Metronom(int, bool).
      */
     auto waitForNextCycle() -> void;
 
 private:
-    int cycleDuration; //!< The duration of one cycle of the loop.
-    /**
-     * In case a cycle has been missed - due to stuff happening within the loop
-     * taking more time than the defined cycle duration - this bool defines
-     * whether the missed cycle should be skipped or not.
-     * In this case \c true will make metronom's
-     * \c waitForNextCycle just wait until the next regular cycle,
-     * thereby skipping at least one cycle, whereas \c false will
-     * make metronom's \c waitForNextCycle return immediately and
-     * thereby trigger
-     */
+    Metronom() = default;                  // hide default constructor
+    Metronom(Metronom const &);            // hide copy constructor
+    Metronom &operator=(Metronom const &); // hide assignment operator.
+
+    int cycleDuration;
     bool skipMissedCycles;
 };
